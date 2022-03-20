1fb2e34b738a3476eea5290836be11f1f359bbfa
Kevin Hookumchand <khookumchand@gmail.com>
2022-03-20 12:09:56 -0400

    Added french translation in fr.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
-  resources :customers
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    get 'customers/index'
+    root to: 'pages#home'
+    resources :customers
+  end
 end
